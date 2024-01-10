00770d0c13998267dad62ce0397cd8eff6adb2fa
Eric Lin <eric.yijuin.lin@gmail.com>
2024-01-10 09:21:06 +0800

    initial commit

---
+++ common_modules/config_reader.py
@@ -0,0 +1,8 @@
+import json
+
+def read_config(file_path: str) -> dict:
+    try:
+        with open(file_path, 'r', encoding="utf-8") as fs:
+            return json.load(fs)
+    except FileNotFoundError:
+        quit(f"failed to load credential: {file_path}")
---
+++ projects/info-tech-gacha/app.py
@@ -0,0 +1,2 @@
+from common_modules.config_reader import read_config
+
